67cb23d32b5418230fb5c45c0c6f5704306d6d0a
Hardik <hardikg2907@gmail.com>
2023-04-04 09:23:19 +0530

    Made all event routes

--- server/controllers/eventController.js
+++ server/controllers/eventController.js
@@ -7,3 +7,100 @@
 //   doc.save(callback); // works
 // });
 
+const {Event} = require('../models/eventModel')
+const mongoose = require('mongoose')
+
+// create event
+const createEvent = async (req,res) => {
+    const {title, description, startTime, endTime, venues,status} = req.body;
+
+    try{
+        const event = await Event.create({
+            title,
+            description,
+            startTime,
+            endTime,
+            venues,
+            status
+        })
+
+        if(event) res.status(200).json(req.body)
+
+    }
+    catch(error)
+    {
+    return res.status(404).json({error})
+    }
+}
+
+// get all events
+const getAllEvents = async (req,res) => {
+    
+    try{
+        const response = await Event.find({})
+        
+        res.status(200).json(response)
+    }
+    catch(error)
+    {
+        return res.status(404).json({error})
+    }
+}
+
+//get one event 
+const getEvent = async (req,res) => {
+    const {_id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) {
+        return res.status(404).json({error: 'No such Event'})
+    }
+    const response = await Event.findById(_id);
+
+    if(!response) {
+        return res.status(404).json({error: 'No such Event'})
+    }
+    
+    res.status(200).json(response)
+}
+
+// updating event
+const updateEvent = async (req,res) => {
+    const {_id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) {
+        return res.status(404).json({error: 'No such Event'})
+    }
+    const response = await Event.findOneAndUpdate({_id},{
+        ...req.body
+    });
+
+    if(!response) {
+        return res.status(404).json({error: 'No such Event'})
+    }
+    
+    res.status(200).json(response)
+}
+
+// deleting event
+const deleteEvent = async (req,res) => {
+    const {_id} = req.params
+
+    if(!mongoose.Types.ObjectId.isValid(_id)) {
+        return res.status(404).json({error: 'No such Event'})
+    }
+    const response = await Event.findByIdAndDelete(_id);
+
+    if(!response) {
+        return res.status(404).json({error: 'No such Event'})
+    }
+    
+    res.status(200).json(response)
+}
+
+module.exports = {
+    createEvent,
+    getAllEvents,
+    getEvent,
+    updateEvent,
+    deleteEvent
+}
--- server/routes/events.js
+++ server/routes/events.js
@@ -1,9 +1,17 @@
 const express = require('express');
+const {createEvent, getAllEvents, getEvent, updateEvent, deleteEvent} = require('../controllers/eventController')
 
 const router = express.Router();
 
 //create a new event
-router.post('/events',)
+router.post('/events',createEvent)
 
+router.get('/events', getAllEvents)
+
+router.get('/event/:_id', getEvent)
+
+router.patch('/event/:_id', updateEvent)
+
+router.delete('/event/:_id', deleteEvent)
 
 module.exports = router
