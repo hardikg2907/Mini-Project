0c2e68f1a3532b99568822cb752f65d0e2a7e581
deepak-ganwani <deeganwani@gmail.com>
2023-05-26 14:53:40 +0530

    Table Alignment Done and Dusted

--- frontend/src/pages/AllEvents.jsx
+++ frontend/src/pages/AllEvents.jsx
@@ -1,18 +1,48 @@
 import { PermissionsTable } from "../components/PermissionsTable";
 import { useEventContext } from "../context/EventContext";
 import { EventDetail } from "../components/EventDetail";
-// import {Calendar} from "../components/Calendar";
+import Calendar from "../components/Calendar";
 import React, { useEffect, useState } from 'react';
 import moment from 'moment'
+import axios from "axios";
 
 const AllEvents = () => {
 
-    const { setAllEvents, allEvents, showModal, setShowModal, setSelectedEvent } = useEventContext()
+    const { setAllEvents, allEvents, showModal, setShowModal, setSelectedEvent, calendarEvents, setCalendarEvents } = useEventContext()
     const [events, setEvents] = useState([])
+    // const [calendarEvents, setCalendarEvents] = useState([])
 
     useEffect(() => {
-        setEvents(allEvents.filter((event) => event.status == 'approved').sort((a,b)=>new Date(b.endTime).getTime()-new Date(a.endTime).getTime()))
+        setEvents(allEvents.filter((event) => event.status == 'approved').sort((a, b) => new Date(b.endTime).getTime() - new Date(a.endTime).getTime()))
+        setCalendarEvents(events.map((element) => {
+            return {
+                title: element.title,
+                startDate: element.startTime,
+                endDate: element.endTime,
+                id: element._id
+            }
+        }))
         console.log(events)
+        // const fetchData = async () => {
+        //     const response = await axios.get('/api/events?status=approved')
+        //     const data = response.data
+        //     // console.log(data);
+
+        //     setCalendarEvents(data.map((element) => {
+        //         return {
+        //             title: element.title,
+        //             startDate: element.startTime,
+        //             endDate: element.endTime,
+        //             id: element._id
+        //         }
+        //     }))
+        //     console.log(calendarEvents)
+
+        //     // console.log(events)
+        //     // setIsFetched(true)
+
+        // }
+        // fetchData()
     }, [])
 
     const selectEvent = (event) => {
@@ -24,7 +54,7 @@ const AllEvents = () => {
         <div className='pageHeader'>
             <div className="permHeader"><h2>All Events</h2></div>
             {/* <PermissionsTable events={allEvents.filter((event)=>event.status=='approved')}/> */}
-            <div className="table">
+            <div className="table allEventsTable">
                 <div className="headings eventHeading">
                     <div className="hehe">Committee</div>
                     <div className="hehe">Event Name</div>
@@ -45,8 +75,8 @@ const AllEvents = () => {
                 </div>
             </div>
             {showModal && <EventDetail allEvents={true} />}
-            {/* <div className="calendar">
-                <Calendar events={events} />
+            {/* <div className="allEventsCalendar">
+                <Calendar events={calendarEvents} />
             </div> */}
         </div>
     );
